41defdacfd61b8aed54d3f8e434138b368f0cb64
David Royer <droyer01@gmail.com>
2017-11-02 22:05:39 -0400

    update

--- pages/classes.vue
+++ pages/classes.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="container">
-    <hero heading="Group Dance Classes" image="/hero-classes.jpg"></hero>
+    <hero heading="Group Dance Classes" :image="require('~/assets/images/hero-classes.jpg')"></hero>
 
     <div class="content">
+      <hero heading="Group Dance Classes" image="/hero-classes.jpg"></hero>
+
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
