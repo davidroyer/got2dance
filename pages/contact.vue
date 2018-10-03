3a8079074e9c92f9ae2e693a14ce5475129f2932
David Royer <droyer01@gmail.com>
2018-10-03 02:56:31 -0400

    added netlify form in contact page and thank-you page to go with it

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,34 @@
 <template>
-  <section class="container">
-    <h1>Contact</h1>
-  </section>
+  <div class="container">
+    <h1 class="title">
+      Contact
+    </h1>
+    <div class="content">
+      <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+        <input type="hidden" name="form-name" value="contact" />
+        <p class="hidden">     
+          <label>Don’t fill this out: <input name="bot-field"></label>   
+        </p>
+        <label class="form-label" for="name">
+          Name:
+        </label>
+        <input class="form-field" name="name" id="name" />
+        <label class="form-label" for="email">
+          Email:
+        </label>
+        <input class="form-field" name="email" id="email" />
+        <label class="form-label" for="message">
+          Message:
+        </label>
+        <textarea class="form-field" name="message" id="message"></textarea>
+        <input class="form-button" type="submit" value="Send message" />
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style lang="scss">
---
+++ pages/thank-you.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="content">
+    <h1 class="title">
+      Thank you
+    </h1>
+    <p class="text-center">
+      <nuxt-link to="/">Back to home</nuxt-link >
+    </p>
+  </div>  
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css">
+</style>
