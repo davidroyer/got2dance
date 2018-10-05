7edf8a7b3cdb96a264e1b2acb6359ee374f5ee6d
David Royer <droyer01@gmail.com>
2018-10-05 14:42:25 -0400

    fixed classname

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
         </div>
       </nuxt-link>
     </div>
-    <section class="map">
+    <section class="section-map">
       <iframe width="600" height="450" frameborder="0" style="border:0" :src="`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJt4KeH5KfaYgRHuLYoPAAkLU&key=${key}`" allowfullscreen></iframe>
     </section>
   </div>
