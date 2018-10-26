6f2cacad79514f6b64f72692952c0c1b0d408e13
David Royer <droyer01@gmail.com>
2018-10-26 00:30:03 -0400

    removed scripts directory

--- scripts/create-env.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-let envContents = ''
-
-for (let varName in process.env) {
-  envContents += `${varName}=${process.env[varName]}\n`
-  console.log(process.env[varName])
-}
-fs.writeFileSync('./.env', envContents)
