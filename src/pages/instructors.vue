2c2e37f3d5299bc797fad41844b0e482bf346c15
David Royer <droyer01@gmail.com>
2020-10-04 19:59:06 -0400

    WIP

--- src/pages/instructors.vue
+++ src/pages/instructors.vue
@@ -2,10 +2,13 @@
   <section class="container">
     <h1 class="page-title">Our Dance Instructors</h1>
     <div class="content">
-
       <div class="instructors flex-center">
-        <div class="instructor" v-for="(instructor, index) in positionOrder(instructors)" :key="index">
-          <img class="instructor-headshot" :src="require(`@/assets/images/instructors/${instructor.headshot}`)" alt="">
+        <div class="instructor" v-for="instructor in instructorsData" :key="instructor._id">
+          <SanityImage
+            class="instructor-headshot"
+            :asset-id="instructor.image.asset._ref"
+            auto="format"
+          />
           <div class="instructor-info">
             <h3 class="instructor-name" v-html="instructor.name"></h3>
             <div v-html="instructor.bio"></div>
@@ -19,15 +22,28 @@
 <script>
 import Hero from '@/components/Hero.vue'
 import instructors from '@/data/instructors.json'
+import { groq } from '@nuxtjs/sanity'
+
+const query = groq`*[_type == "instructor"] {
+  _id,
+  name,
+  displayOrder,
+  image,
+  bio
+}`
 
 export default {
   head () {
     return this.$createSeo('instructors')
   },
 
-  async asyncData ({ app, payload, params }) {
+  async asyncData ({ app, $sanity }) {
+    const instructorsData = await $sanity.fetch(query)
+    instructorsData.sort((a, b) => a.displayOrder - b.displayOrder)
+
     return {
-      instructors
+      instructors,
+      instructorsData
     }
   },
 
@@ -48,45 +64,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.instructor {
-  flex-flow: row wrap;
-  justify-content: space-around;
-  margin-bottom: 5em;
+  .instructor {
+    flex-flow: row wrap;
+    justify-content: space-around;
+    margin-bottom: 5em;
 
-  @media (min-width: 680px) {
-    display: flex;
-  }
-  &-headshot {
-    max-width: 300px;
-    border-radius: 400px;
-    border: 2px solid lightgray;
-    flex: 1 1 300px;
-    height: auto;
-    align-self: flex-start;
-    max-width: 200px;
-    justify-content: center;
-    margin-left: auto;
-    display: block;
-    margin-right: auto;
+    @media (min-width: 680px) {
+      display: flex;
+    }
+    &-headshot {
+      max-width: 300px;
+      border-radius: 400px;
+      border: 2px solid lightgray;
+      flex: 1 1 300px;
+      height: auto;
+      align-self: flex-start;
+      max-width: 200px;
+      justify-content: center;
+      margin-left: auto;
+      display: block;
+      margin-right: auto;
 
-    @media (min-width: 980px) {
-      max-width: 250px;
+      @media (min-width: 980px) {
+        max-width: 250px;
+      }
+    }
+
+    &-info {
+      text-align: left;
+      margin-left: auto;
+      flex: 0 1 calc(100% - 350px);
+    }
+
+    &-name {
+      font-size: 1.7em;
+      margin-bottom: 0.25em;
+      @media (max-width: 679px) {
+        margin-bottom: 0.75em;
+        text-align: center;
+      }
     }
   }
-
-  &-info {
-    text-align: left;
-    margin-left: auto;
-    flex: 0 1 calc(100% - 350px);
-  }
-
-  &-name {
-    font-size: 1.7em;
-    margin-bottom: 0.25em;
-    @media (max-width: 679px) {
-      margin-bottom: 0.75em;
-      text-align: center;
-    }
-  }
-}
 </style>
